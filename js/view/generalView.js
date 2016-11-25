62508b26c8e786700f1238eb3c809cc71919d377
wwwal2 <smalex9@gmail.com>
2016-11-25 20:54:25 +0200

    version 1

--- js/view/generalView.js
+++ js/view/generalView.js
@@ -1,4 +1,11 @@
 "use strict";
 
 function GeneralView(core) {
+	
+	var numbers = $(".numbers");
+	
+	numbers.onclick(function(event) {
+		var pushedButton = event.target.value;
+		core.value = pushedButton.value;
+	});
 }
