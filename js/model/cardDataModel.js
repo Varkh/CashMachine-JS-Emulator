6db2f61c471f278c03283d9f3ae6f3b50a191366
Dmitry Shapovalov <flami127@gmail.com>
2016-11-26 12:58:49 +0200

    +new check card metod

--- js/model/cardDataModel.js
+++ js/model/cardDataModel.js
@@ -29,13 +29,13 @@ function CardDataModel() {
      * should be changed to hash instead
      * or used only for card creation
      */
-    this._pin = [1,1,1,1];
+    this.defaultPin = [1,1,1,1];
 
     /**
      * ExpirationDate
      * Not allowed to use card with expired date
      */
-    this._expirationDate = [30,1,18];
+    this._expirationDate = [30,1,2018];
 }
 
 
@@ -54,12 +54,38 @@ CardDataModel.prototype.getBallance = function () {
 
 CardDataModel.prototype.checkPin = function (inputPin) {
     for (var i = 0; i < inputPin.length; i++) {
-        if (inputPin[i] !== this._pin[i])return false;
+        if(this.defaultPin){
+            this.pin = this.defaultPin;
+        }
+        if (inputPin[i] !== this.pin[i]) {
+            return false
+        }
     }
         return true;
 };
 
-/*CardDataModel.prototype.isNotExpired = function () {
-  var now = new Date(milliseconde);
-  var expDate =
-};*/
+CardDataModel.prototype.setPin = function (newPin) {
+    if (this.defaultPin){
+        delete this.defaultPin
+    }
+    this.pin = newPin;
+};
+
+CardDataModel.prototype.isNotExpired = function () {
+    var now = new Date();
+    if(now.getFullYear() <= this._expirationDate[2]
+        && now.getMonth() < this._expirationDate[1]
+        && now.getDate() < this._expirationDate[0]) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+CardDataModel.prototype.isCardOk = function (inputPin) {
+    if (this.checkPin(inputPin) && this.isNotExpired()){
+        return true;
+    } else {
+        return false;
+    }
+};
