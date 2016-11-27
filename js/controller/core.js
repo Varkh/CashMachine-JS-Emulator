6105996be3936f5c8626e98b3772d7a9178494d0
Kovaluov Dmitriy <dima_kovaluov@mail.ru>
2016-11-27 08:06:01 +0200

    Revert "some core code"
    This reverts commit 7f905d1c4e4958cf6d52bc848f1cba1edfcab3fe.

--- js/controller/core.js
+++ js/controller/core.js
@@ -6,71 +6,25 @@
  * @constructor
  */
 function Core(cashModule, cardModule) {
-    this.pin='';
-    this.sumCash='';
-    this.output="Waiting for card";
-    this.cardInserted=false;
-    this.pinChecked=false;
-    this.cardChecked=false;
-
+    console.log("Waiting for card");
 
     this.pushCard = function (cardData) {
-        if (cardData) {
-            this.cardInserted=true;
-            this.output="Enter pin";
-        }else {
-            this.cardInserted=false;
-            this.pinChecked=false;
-            this.cardChecked=false;
-        }
+        console.log("Enter pin");
     };
-
-    this.checkCardDate = function () {
-        var date=new Date ()
-       var dateM=date.getMonth();
-        var dateY=+1+'/'+date.getFullYear();
-       if (new Date (dateY,dateM)< cardModule.readCard('date')){
-           this.output="Card out of date!";
-       }
-    }
 
     this.enterPin = function (button) {
-        if ($.isNumeric(button)&&this.pin.length<4) {
-            this.pin=this.pin+button;
+
+        if (isNumeric(button.value)) {
+            var pin=toString(button)+button;
         }
 
-        if (this.pin.length===4&&button==='Submit') {
-           var chekPin = cardModule.getPin();
-            if (this.pin==chekPin) {
-                    this.pinChecked=true;
-            } else {this.output='Incorrect pin'}
-
-        }else if (button==='Cancel'){this.pin=''}
-
-    };
-
-    this.enterSumCash = function (Cash) {
-        if ($.isNumeric(button)) {
-            this.sumCash=this.sumCash+button;
+        if (pin.length===4&&button.value==='Submit') {
+            //введен пин проверить
         }
-
-        if (button==='Submit') {
-            this.getCash();
-        } else if (button==='Cancel') {this.sumCash=''}
+        console.log("Autorized.")
     };
 
-    this.getOutput = function () {
-        return this.output;
-    }
-
-    this.getCash=function (money) {
-        var isMoney=cardModule.getBalance;
-        if (isMoney<money) {this.output='No money'; return;}
-        if (this.cardInserted&&this.pinChecked&&this.cardChecked) {
-            if (cashModule.checkMoney(money)) {
-                cashModule.getCash(money);
-            }else this.output='There is not enought money';
-        } else {this.output='Strange error'}
-    }
-
+    this.getCash = function(n) {
+        return {100: 2, 500: 1};
+    };
 }
