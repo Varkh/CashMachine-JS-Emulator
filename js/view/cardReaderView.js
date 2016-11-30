fa36efa034a02f6546a324431e4ef057cb9a1435
juliakukuliak <juliakukuliak@gmail.com>
2016-11-30 19:38:08 +0200

    Card Reader View; Display View

--- js/view/cardReaderView.js
+++ js/view/cardReaderView.js
@@ -1,2 +1,8 @@
 "use strict";
-
+function CardReaderView() {
+	var cardButton = document.getElementById("getCard");
+	var event = new Event('card-inserted');
+	cardButton.onclick = function() {
+		document.body.dispatchEvent(event);
+	}
+}
--- js/view/displayView.js
+++ js/view/displayView.js
@@ -1,1 +1,16 @@
 "use strict";
+var display = document.getElementById("display");
+function DisplayView() {
+	this.message = document.createElement("DIV");
+	this.showHello = function(text) {
+		this.message.innerText = text;
+		display.appendChild(this.message);
+	}
+
+	this.showInput = function(numbers) {
+		this.message.innerText = numbers;
+		display.appendChild(this.message);
+	}
+}
+var a = new DisplayView().showHello("showHello");
+console.log(a);
