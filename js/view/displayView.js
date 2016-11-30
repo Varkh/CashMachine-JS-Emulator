9916b1cbf77051ef04103727dc678aee0f278caf
wwwal2 <smalex9@gmail.com>
2016-11-30 14:28:27 +0200

    Display buttons baseVersion

--- js/view/displayView.js
+++ js/view/displayView.js
@@ -1,5 +1,14 @@
 "use strict";
 
 function DisplayView() {
+    var displayButtons = document.getElementsByClassName("empty");
+    var displaySpace = document.getElementById("display");
     
+    for (var i = 0; i < displayButtons.length; i++) {
+    	displayButtons[i].textContent = i + 1;
+    	displayButtons[i].onclick = function(event) {
+			var entered = event.target.textContent;
+			console.log(entered);
+		}
+    }
 }
