70365e3d7bcf32c9dda7deb5ac1be3873515fe08
wwwal2 <smalex9@gmail.com>
2016-12-02 20:13:03 +0200

    bugFix

--- js/view/displayView.js
+++ js/view/displayView.js
@@ -1,20 +1,7 @@
 "use strict";
 
 function DisplayView() {
-<<<<<<< HEAD
-    var displayButtons = document.getElementsByClassName("empty");
-    var displaySpace = document.getElementById("display");
-    displaySpace.textContent = "INSERT CARD";
 
-    for (var i = 0; i < displayButtons.length; i++) {
-    	displayButtons[i].textContent = i + 1;
-    	displayButtons[i].onclick = function(event) {
-			var entered = event.target.textContent;
-			displaySpace.textContent = entered + " option selected";
-		}
-    }
-}
-=======
 	var display = document.getElementById("display");
 	this.message = document.createElement("DIV");
 	this.showMessage = function(text) {
@@ -42,4 +29,3 @@ function DisplayView() {
 	  }
 	}
 }
->>>>>>> af25249cf693e193c1f82f9b488c1ba4fe65207f
--- js/view/generalView.js
+++ js/view/generalView.js
@@ -9,14 +9,4 @@ function GeneralView(core) {
 	$ctrl.find('#clear').click(core.onClearBtnClick);
 	$ctrl.find('#submit').click(core.onSubmitBtnClick)
 
-<<<<<<< HEAD
-	for (var i = 0; i < numbers.length; i++) {
-		
-		numbers[i].onclick = function(event) {
-			core = event.target.innerText;
-			console.log(core);
-		}
-	}
-=======
->>>>>>> af25249cf693e193c1f82f9b488c1ba4fe65207f
 }
