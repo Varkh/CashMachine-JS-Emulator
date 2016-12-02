95e87095320a54de263de43d9b04b2ef0390c30f
juliakukuliak <juliakukuliak@gmail.com>
2016-12-02 20:46:40 +0200

    d

--- js/view/cardLayoutView.js
+++ js/view/cardLayoutView.js
@@ -8,7 +8,7 @@ function CardLayoutView(wallet) {
         if (e.keyCode == 13){
             var newcard = new CardDataModel();
             newcard._holderName = this.value;
-            var newCardView = $('div');
+            var newCardView = $("<div>");
             newCardView.className = "new-card";
             newCardView.Data = newcard;
             this.parent.appendChild(newCardView);
